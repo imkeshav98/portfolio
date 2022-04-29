fce7b49b82b59ba35d5df597fdca3544701e584c
imkeshav98 <imkeshav98@gmail.com>
2022-04-29 07:13:40 +0530

    social links

--- src/components/header/HeaderSocial.jsx
+++ src/components/header/HeaderSocial.jsx
@@ -5,10 +5,18 @@ export const HeaderSocial = () => {
   return (
     <div>
       <div className="header__socials">
-        <a href="https://linkedin.com" target="_blank" rel="noreferrer">
+        <a
+          href="https://linkedin.com/keshav-k-chaudhary/"
+          target="_blank"
+          rel="noreferrer"
+        >
           <BsLinkedin />
         </a>
-        <a href="https://github.com" target="_blank" rel="noreferrer">
+        <a
+          href="https://github.com/imkeshav98"
+          target="_blank"
+          rel="noreferrer"
+        >
           <FaGithub />
         </a>
       </div>
