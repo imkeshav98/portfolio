1826fb6a1575cd6005e9a1a06b685d2d1b980347
imkeshav98 <imkeshav98@gmail.com>
2022-04-30 18:59:30 +0530

    CTA Update

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -3,7 +3,7 @@ import CV from "../../assets/keshav-chaudhary-resume.pdf";
 export const CTA = () => {
   return (
     <div className="cta">
-      <a href={CV} download className="btn">
+      <a href={CV} download className="btn" target="_blank" rel="noreferrer">
         Download Resume
       </a>
       <a href="#contact" className="btn btn-primary">
